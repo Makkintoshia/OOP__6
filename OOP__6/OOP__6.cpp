2909654e9eff14e99dfc2e1836020d8a80fbc4c2
Makkintoshia <85276507+Makkintoshia@users.noreply.github.com>
2021-08-02 19:23:45 +0300

    Добавьте файлы проекта.

---
+++ OOP__6/OOP__6.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+
+
+
+
+
+template<typename T, typename K>
+K swap(T i, T j,K m )
+{
+
+	int t;
+	for ( i = 0; i <5; i++)
+	{
+		for ( j = i; j <5; j++)
+		{
+			t = m[i][j];
+		m[i][j] = m[j][i];
+			m[j][i] = t;
+			
+		}
+		
+	}
+	return m;
+}
+
+
+
+int main()
+{
+	const int i = 5;
+	const int j = 5;
+	int m[5][5] =
+	{
+		{1,2,3,4,5},
+		{6,7,8,9,10},
+		{11,12,13,14,15},
+		{16,17,18,19,20},
+		{21,22,23,24,25}
+	};
+	//Matrix matrix(3, 3);//
+
+	swap(5,5,m);
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+			cout << m[i][j]<<"  ";
+		cout << '\n';
+	}
+	return 1;
+}
+	  
